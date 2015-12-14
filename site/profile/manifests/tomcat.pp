1997807ef9827044e9ae866e02d33240856e1e1f
Viacheslav Dubrovskyi <dubrsl@gmail.com>
2015-12-14 15:01:51 +0200

    Add instances wf-central & wf-region

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -7,14 +7,29 @@ class profile::tomcat {
 
     class { '::tomcat':}
     include ::java
-    tomcat::instance { 'default':
+    tomcat::instance { 'wf-central':
 	install_from_source => false,
 	package_name        => 'tomcat',
+	catalina_base => '/var/lib/tomcat/wf-central',
     }->
 
-    tomcat::service { 'default':
+    tomcat::service { 'wf-central':
 	use_jsvc     => false,
 	use_init     => true,
-	service_name => 'tomcat',
+	service_name => 'tomcat_wf-central',
+	catalina_base => '/var/lib/tomcat/wf-central',
     }
+
+    tomcat::service { 'wf-region':
+	use_jsvc     => false,
+	use_init     => true,
+	service_name => 'tomcat_wf-region',
+	catalina_base => '/var/lib/tomcat/wf-region',
+    }
+
+    firewall { '101 HTTP inbound':
+	dport  => 8080,
+	proto  => tcp,
+	action => accept,
+  }
 }
